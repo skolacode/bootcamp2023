2e28c23c88efb66911eada6e6a8fa0f39fadf7c4
Mohamed Burhan <bern2727@gmail.com>
2023-09-05 22:00:12 +0800

    FEAT: Implement a login function

--- js/users.js
+++ js/users.js
@@ -2,8 +2,6 @@ function saveUser(username, password) {
   const userData = localStorage.getItem('users')
   let users = JSON.parse(userData)
 
-  console.log('users: ', users)
-
   if(users && users.length >= 0) {
 
     // if users key exist, just append a new value into the array
@@ -28,4 +26,27 @@ function saveUser(username, password) {
 
   // Save the latest value into the localstorage
   localStorage.setItem('users', JSON.stringify(users))
+}
+
+function getUser(username, password) {
+  const userData = localStorage.getItem('users')
+  let users = JSON.parse(userData)
+
+  if(users && users.length > 0) {
+    // users is an array
+
+    const user = users.find((userObj) => {
+      if(userObj.name === username && userObj.password === password) {
+        return userObj
+      }
+    })
+
+    if(user) {
+      localStorage.setItem('loginUser', JSON.stringify(user))
+      return true
+    }
+
+    alert('Username and password is not match')
+    return false
+  }
 }
