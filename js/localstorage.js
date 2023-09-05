24b3dc296b4824b204b257ed749310305148026b
Mohamed Burhan <bern2727@gmail.com>
2023-09-05 20:41:39 +0800

    CHORE: Delete unused file

--- js/localstorage.js
+++
@@ -1,8 +0,0 @@
-function saveData(key, value) {
-  localStorage.setItem(key, JSON.stringify(value))
-}
-
-function getData(key) {
-  const data = localStorage.getItem(key)
-  return JSON.parse(data)
-}
