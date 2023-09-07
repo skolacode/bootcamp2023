55d456dbe0a6cad74f93499071b8e1838b362fad
Mohamed Burhan <bern2727@gmail.com>
2023-09-07 21:21:36 +0800

    CHORE: Data structure

---
+++ dataDesign.js
@@ -0,0 +1,21 @@
+// This is the Data structure in Local Storage
+
+const userLogin = {
+  name: '',
+}
+
+const users = [
+  {
+    name: '', // This must be unique
+    password: ''
+  }
+]
+
+const images = [
+  {
+    imgName: '',
+    img: '',
+    username: '',
+    isPublic: "true or false "
+  }
+]
